c53f75bb24312960a55c7f32f9e91c1c22dd8301
Martin Stolk <mrtnstolk@gmail.com>
2014-10-29 23:32:37 +0200

    Online Highscores

--- Eclipse/Jumpy/src/com/example/jumpy/HighScoreActivity.java
+++ Eclipse/Jumpy/src/com/example/jumpy/HighScoreActivity.java
@@ -3,12 +3,25 @@ package com.example.jumpy;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 
 public class HighScoreActivity extends Activity 
 {
+	Context context = this;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -23,6 +36,37 @@ public class HighScoreActivity extends Activity
 
 		HighScoreAdapter adapter = new HighScoreAdapter(this, items);
 		highscores.setAdapter(adapter);
+		
+		Button online = (Button)findViewById(R.id.btnOnline);
+		online.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				ParseQuery<ParseObject> query = ParseQuery.getQuery("Highscore");
+				query.orderByAscending("UserScore");
+				query.setLimit(10);
+				query.findInBackground(new FindCallback<ParseObject>() {
+
+					@Override
+					public void done(List<ParseObject> arg0, ParseException arg1) {
+						// TODO Auto-generated method stub
+						List<HighScore> onlineitems = new ArrayList<HighScore>();
+						
+						for (int i = 0; i < arg0.size(); i++)
+						{
+							ParseObject po = arg0.get(i);
+							onlineitems.add(new HighScore(0, po.getString("UserName"), 0, po.getInt("UserScore")));
+						}
+						
+						ListView highscores = (ListView)findViewById(R.id.lvHighScores);
+						HighScoreAdapter adapter = new HighScoreAdapter(context, onlineitems);
+						highscores.setAdapter(adapter);
+					}
+				});
+				
+			}
+		});
 
 	}
 }
